83c4d03423657984738035cc81eca554c1985d94
shampuronline <afkmamunbd3@gmail.com>
2024-12-28 10:40:10 +0600

    Bumps version to X.Y.Z

--- update_version.sh
+++ update_version.sh
@@ -5,14 +5,16 @@
 
 git flow release start v$1
 
-sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" newfies/newfies_dialer/__init__.py
+sed - i - e "s/__version__ = '.*'/__version__ = '$1'/g" newfies / newfies_dialer / __init__.py
 
-#rm -rf docs/html
-#python setup.py develop
-#make docs
+# rm -rf docs/html
+# python setup.py develop
+# make docs
 
-git commit -a -m "Update to version v$1"
+git commit - a - m "Update to version v$1"
 git flow release finish v$1
-#python setup.py sdist upload -r pypi
+# python setup.py sdist upload -r pypi
 
-git push origin develop; git push origin master; git push --tags
+git push origin develop
+git push origin master
+git push - -tags
